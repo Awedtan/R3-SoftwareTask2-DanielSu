b41dbaa923c663ce804cda1fbbd284b4cdd1ab0d
Awedtan <31903483+Awedtan@users.noreply.github.com>
2021-10-24 21:01:23 -0400

    get the proper motor outputs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, time, math
 pygame.init()
 
 HORIZONTAL, VERTICAL = 0, 1
-DEADZONE = 0.2
+DEADZONE = 0.1
 
 joysticks = [pygame.joystick.Joystick(i) for i in range(pygame.joystick.get_count())]
 for joystick in joysticks:
@@ -12,61 +12,78 @@ for joystick in joysticks:
 # 0 - horizontal, positive is right
 # 1 - vertical, positive is down
 
-joystick_values = {0:0, 1:0, 2:0, 3:0}
+joystick_values = {0: 0, 1: 0, 2: 0, 3: 0}
 
 while True:
-	
+
 	isMoving = False
-	
+
 	for event in pygame.event.get():
 		if event.type == pygame.JOYAXISMOTION:
-			if(abs(event.value) > DEADZONE ):
+			if(abs(event.value) > DEADZONE):
 				joystick_values[event.axis] = event.value
 			else:
 				joystick_values[event.axis] = 0
-	
-	
+
 	for key in joystick_values:
 		if joystick_values[key] != 0:
 			isMoving = True
-	
+
 	right = joystick_values[0] >= DEADZONE
 	forward = -joystick_values[1] >= DEADZONE
-	
-	print(joystick_values)
-	print(isMoving)
-	print("r:" + str(right))
-	print("f:" + str(forward))
-	print()
-	
+
+	# print(joystick_values)
+	# print(isMoving)
+	# print("r:" + str(right))
+	# print("f:" + str(forward))
+	# print()
+
 	motor_out = ["0", "0", "0", "0"]
-	
+
 	if(not isMoving):
 		continue
-	
+
 	magnitude = math.sqrt(joystick_values[0]**2 + joystick_values[1]**2)
 	if(magnitude > 1):
 		magnitude = 1
-	
 	speed = int(magnitude*255)
-	
+
+	angle = 1
+	if(joystick_values[0] != 0):
+		angle = (abs(math.atan(joystick_values[1]/joystick_values[0])) - math.pi/4) / (math.pi/4)
+
 	if right:
 		if forward:
-			motor_out[0] = "f255"
-			# motor_out[2] = something
+			motor_out[0] = "f" + str(speed)
+
+			temp = int(angle * speed)
+			motor_out[2] = "f" if temp >= 0 else "r"
+			motor_out[2] += str(abs(temp))
+
 		else:
-			# motor_out[0] = something
-			motor_out[2] = "r255"
+			temp = int(angle * speed)
+			motor_out[0] = "r" if temp >= 0 else "f"
+			motor_out[0] += str(abs(temp))
+
+			motor_out[2] = "r" + str(speed)
+
 	elif forward:
-		# motor_out[0] = something
-		motor_out[2] = "f255"
+		temp = int(angle * speed)
+		motor_out[0] = "f" if temp >= 0 else "r"
+		motor_out[0] += str(abs(temp))
+
+		motor_out[2] = "f" + str(speed)
+
 	else:
-		motor_out[0] = "r255"
-		# motor_out[2] = something
-	
-	motor_out[1] = motor_out[0]
-	motor_out[3] = motor_out[2]
-	
+		motor_out[0] = "r" + str(speed)
+
+		temp = int(angle * speed)
+		motor_out[2] = "r" if temp >= 0 else "f"
+		motor_out[2] += str(abs(temp))
+
+	for x in range(1,4,2):
+		motor_out[x] = motor_out[x-1]
+
 	print(motor_out)
-		
+
 	time.sleep(0.4)
